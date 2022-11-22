0542e9a3c3ff7f6c892ac28080d62bdef55cf38a
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2022-11-22 21:50:49 +0000

    Patch: win icons
    Show window icon to the left of window title if available.
    Requires imlib2.
    
    Modified to work with bar modules patch.

--- patch/bar_wintitle.c
+++ patch/bar_wintitle.c
@@ -14,7 +14,8 @@ draw_wintitle(Bar *bar, BarDrawArg *a)
 
 	if (m->sel) {
 		drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-		drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+        drw_text(drw, x, 0, w, bh, lrpad / 2 + (m->sel->icon ? m->sel->icw + ICONSPACING : 0), m->sel->name, 0);
+        if (m->sel->icon) drw_pic(drw, x + lrpad / 2, (bh - m->sel->ich) / 2, m->sel->icw, m->sel->ich, m->sel->icon);
 		if (m->sel->isfloating)
 			drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 	} else {
