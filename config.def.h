e0e6dbdbdddcbcb4a79cd3810cf5b93fec3ebe61
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2022-11-23 22:54:09 +0000

    Fix bar module positions
    status button -> layout symbol -> window title -> extra status text ->
    tags -> status text

--- config.def.h
+++ config.def.h
@@ -66,10 +66,10 @@ static const BarRule barrules[] = {
 	/* monitor  bar    alignment               widthfunc              drawfunc              clickfunc           name */
 	{ -1,       0,     BAR_ALIGN_CENTER,       width_tags,            draw_tags,            click_tags,         "tags" },
 	{ -1,       0,     BAR_ALIGN_LEFT_LEFT,    width_stbutton,        draw_stbutton,        click_stbutton,     "statusbutton" },
-	{ -1,       0,     BAR_ALIGN_RIGHT_LEFT,   width_ltsymbol,        draw_ltsymbol,        click_ltsymbol,     "layout" },
-	{ -1,       0,     BAR_ALIGN_LEFT,         width_wintitle,        draw_wintitle,        click_wintitle,     "wintitle" },
-	{ -1,       0,     BAR_ALIGN_LEFT,         width_status2d,        draw_status2d,        click_statuscmd,    "status2d" },
-	{ -1,       0,     BAR_ALIGN_RIGHT_RIGHT,  width_status2d_es,     draw_status2d_es,     click_statuscmd_es, "status2d_es" },
+	{ -1,       0,     BAR_ALIGN_LEFT_LEFT,    width_ltsymbol,        draw_ltsymbol,        click_ltsymbol,     "layout" },
+	{ -1,       0,     BAR_ALIGN_LEFT_LEFT,    width_wintitle,        draw_wintitle,        click_wintitle,     "wintitle" },
+	{ -1,       0,     BAR_ALIGN_LEFT_LEFT,    width_status2d_es,     draw_status2d_es,     click_statuscmd_es, "status2d_es" },
+	{ -1,       0,     BAR_ALIGN_RIGHT_RIGHT,  width_status2d,        draw_status2d,        click_statuscmd,    "status2d" },
 };
 
 /* layout(s) */
