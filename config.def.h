f4333b8f6d984504a8d41f301fad207e3fc04233
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2022-11-26 00:16:57 +0000

    Patch: move client up/down stack with bindings

--- config.def.h
+++ config.def.h
@@ -135,6 +135,7 @@ ResourcePref resources[] = {
 };
 
 
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,             spawn,          {.v = dmenucmd } },
@@ -148,6 +149,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,             incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_comma,         setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_period,        setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,             movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,             movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Return,        zoom,           {0} },
 	{ MODKEY,                       XK_Tab,           view,           {0} },
 	{ MODKEY,                       XK_q,             killclient,     {0} },
