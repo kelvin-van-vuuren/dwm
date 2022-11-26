b60f95525f3ae4ad58873c8d73cc70f0dc060729
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2022-11-26 00:12:46 +0000

    Patch: cycle layouts with mod+shift+h/l

--- config.def.h
+++ config.def.h
@@ -94,6 +94,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -155,6 +156,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_m,             setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,         setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,         togglefloating, {0} },
+	{ MODKEY|ShiftMask,		        XK_h,             cyclelayout,    {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_l,             cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_0,             view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,             tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,         focusmon,       {.i = -1 } },
