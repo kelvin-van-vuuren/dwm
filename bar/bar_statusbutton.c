0759972d6c41904bd73bc8ee951b5ea28ddef2f2
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-12-02 13:05:03 +0000

    Always highlight status button of focused monitor
    Previously status button of focused monitor only highlighted if a client
    was selected. This meant that when no clients were open on any of the
    monitors it was impossible to tell which one was currently focused.

--- bar/bar_statusbutton.c
+++ bar/bar_statusbutton.c
@@ -9,7 +9,7 @@ draw_stbutton(Bar *bar, BarDrawArg *a)
 {
 	Monitor *m = bar->mon;
 
-    if (bar->mon->sel)
+    if (bar->mon == selmon)
 		drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 	return drw_text(drw, a->x, 0, a->w, bh, lrpad / 2, buttonbar, 0);
 }
